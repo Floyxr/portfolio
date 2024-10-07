487be72fd3270d9f209533bc3eeb70c90b8bcdc3
Floyxr <ciro.cefariello2006@hotmail.com>
2024-10-07 15:11:43 +0200

    Add files via upload

---
+++ Portfolio/PagineJS/index.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <section>
+        <h1>Benvenuto nel mio Portfolio</h1>
+        <p>Esplora i miei progetti e scopri di più su di me.</p>
+      </section>
+    </Layout>
+  );
+}
---
+++ Portfolio/PagineJS/performance.js
@@ -0,0 +1,3 @@
+import Image from 'next/image';
+
+<Image src="/me.jpg" alt="Profile" width={150} height={150} />
